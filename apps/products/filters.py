aac112e5555685dd3c6280cec2155e8cf1a2b919
hamed-nhi <hamed.nahali.opt@gmail.com>
2025-08-04 10:19:58 +0330

    fix the price filter issue

--- apps/products/filters.py
+++ apps/products/filters.py
@@ -1,9 +1,12 @@
 
 
-
 import django_filters
+from .models import Product
 
 class ProductFilter(django_filters.FilterSet):
-    # نام فیلد در مدل
-    # بر اساس کوچکتر یا مساوی از عدد انتخابی کاربر
-    price=django_filters.NumberFilter(field_name='price',lookup_expr='lte')
+    price_min = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    price_max = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    class Meta:
+        model = Product
+        fields = ['price_min', 'price_max']
